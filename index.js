0dfcabf99f82cdf575f01f6a88edfe6c2cf4ef01
jasonronalddavis <jdavis@c.ringling.edu>
2021-08-25 03:05:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(myArray, funcOper){
+    let newArr = []
+    myArray.forEach(element => {
+        newArr.push(funcOper(element))
+    });
+    return newArr
+}
+
+function reduce(myArray, funcOper, startPt) {
+    let total;
+    let i; 
+
+    if (!!startPt) {
+        total = startPt;
+        i = 0;
+    } else {
+        total = myArray[0];
+        i = 1;
+    };
+
+    for( ; i < myArray.length; i++) {
+        total = funcOper(myArray[i], total);
+    };
+    return total;
+} 
